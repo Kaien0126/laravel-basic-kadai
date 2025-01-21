c0eb53391fac75f6af91d9be8d5cf8ce2c1e9802
kaien0126 <kobayan19760126@gmail.com>
2025-01-21 10:31:43 +0900

    課題 Laravel クエリビルダ

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
     public function index() {
-        return view('posts.index');
+        // データ取得
+        $posts = DB::table('posts')->get();
+
+        // indexにデータを渡す
+        return view('posts.index', compact('posts'));
     }
 }
